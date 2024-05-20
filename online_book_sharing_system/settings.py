4c5170521ff13bb0be71c279a71f849bcb72fe9f
Nitay Das <119519860+NitayDas@users.noreply.github.com>
2024-05-20 23:41:54 +0600

    Update settings.py

--- online_book_sharing_system/settings.py
+++ online_book_sharing_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x#w=vz0zi3)16@x77#u4y2k2nvbsw@k&!p0o5rq(@r29-g7f#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
